fa8c51b27b552a8a591c0ec8c4e132966f05efff
Jean Motta <jeanmotta.ti@gmail.com>
2018-07-01 19:00:20 -0300

    (master)

--- GUITask.pas
+++ GUITask.pas
@@ -1,3 +1,3 @@
 Linha criada na master
 Alterado no branch dev-tstGUI
-Esta linha foi adicionada no master e agora alterada para testar o merge pelo Beyond
+Esta linha foi adicionada no master e agora alterada para testar o merge pelo Beyond (master)
